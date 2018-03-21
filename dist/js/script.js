fc7baa22b8971776f20f7f2a942af8482f012125
EugeneAndrievsky <windbojik@yandex.ru>
2018-03-21 12:15:27 +0300

    Сделал вывод данных в модальное окно

--- dist/js/script.js
+++ dist/js/script.js
@@ -10488,7 +10488,7 @@ window.$ = window.jQuery = __WEBPACK_IMPORTED_MODULE_0_jquery___default.a;
 
 	$(function () {
 
-		//-----
+		//-----        
 		$.datepicker.regional['ru'] = {
 			closeText: 'Закрыть',
 			prevText: '&#x3c;Пред',
@@ -10505,33 +10505,60 @@ window.$ = window.jQuery = __WEBPACK_IMPORTED_MODULE_0_jquery___default.a;
 			showMonthAfterYear: false,
 			yearSuffix: ''
 		};
-		$.datepicker.setDefaults($.extend({ 'dateFormat': 'dd/mm/yy' }, $.datepicker.regional['ru']));
+		$.datepicker.setDefaults($.extend({ 'dateFormat': 'dd.mm.yy' }, $.datepicker.regional['ru']));
 		$('#date').datepicker();
 
 		var deliveryCalculator = {
+
 			optionLength: 0,
+			optionWidth: 0,
+			optionHeight: 0,
+			optionWeight: 0,
+			optionDate: 0,
+			optionFast: false,
+			optionFrom: '',
+			optionTo: '',
 
 			init: function init() {
 				var self = this;
 				$('#btnSubmit').on('click', function (e) {
-					e.preventDefault;
+					//e.preventDefault;
 					self.getResults();
-					return false;
+					//return false;
 				});
 			},
 
 			getResults: function getResults() {
 				var self = this;
 				self.optionLength = parseInt($('#length').val()) || 0;
-				// 
-				alert('Длина: ' + self.optionLength + ' см');
+				self.optionWidth = parseInt($('#width').val()) || 0;
+				self.optionHeight = parseInt($('#height').val()) || 0;
+				self.optionWeight = parseInt($('#weight').val()) || 0;
+				self.optionDate = $('#date').val() || 0;
+				self.optionFast = $('#fast').is(':checked');
+				self.optionFrom = $('#from').val() || '';
+				self.optionTo = $('#to').val() || '';
+
+				var modal = $('.modal');
+				modal.find('span[data-length]').html(self.optionLength);
+				modal.find('span[data-width]').html(self.optionWidth);
+				modal.find('span[data-height]').html(self.optionHeight);
+				modal.find('span[data-weight]').html(self.optionWeight);
+				modal.find('span[data-date]').html(self.optionDate);
+				modal.find('span[data-from]').html(self.optionFrom);
+				modal.find('span[data-to]').html(self.optionTo);
+				if (self.optionFast) {
+					modal.find('span[data-fast]').html("срочная");
+				} else {
+					modal.find('span[data-fast]').html('обычная');
+				}
 			}
 
 		};
 
 		deliveryCalculator.init();
 
-		///---
+		///-----
 
 	});
 })(jQuery);
--- src/js/script.js
+++ src/js/script.js
@@ -8,7 +8,7 @@ import 'jquery-ui/ui/widgets/datepicker.js';
 
     $(function() {
 		
-		//-----
+		//-----        
 	$.datepicker.regional['ru'] = {
 		closeText: 'Закрыть',
 		prevText: '&#x3c;Пред',
@@ -29,36 +29,63 @@ import 'jquery-ui/ui/widgets/datepicker.js';
 	};
 	$.datepicker.setDefaults(
 	  $.extend(
-		{'dateFormat':'dd/mm/yy'},
+		{'dateFormat':'dd.mm.yy'},
 		$.datepicker.regional['ru']
 	  )
 	);
 	$('#date').datepicker();
 	
 	var deliveryCalculator = {
+        
 		optionLength: 0,
+        optionWidth: 0,
+        optionHeight: 0,        
+        optionWeight: 0,
+        optionDate: 0,
+        optionFast: false,
+        optionFrom: '',
+        optionTo: '',
 		
 		init: function(){
 			var self = this;
 			$('#btnSubmit').on('click', function(e){
-				e.preventDefault;
+				//e.preventDefault;
 				self.getResults();
-				return false;
+				//return false;
 			});
 		},
 		
 		getResults: function(){
 			var self = this;
 			self.optionLength = parseInt($('#length').val()) || 0;
-			//test 
-			alert('Длина: ' + self.optionLength + ' см');
+            self.optionWidth = parseInt($('#width').val()) || 0;
+            self.optionHeight = parseInt($('#height').val()) || 0;
+            self.optionWeight = parseInt($('#weight').val()) || 0;
+            self.optionDate = $('#date').val() || 0;
+            self.optionFast = $('#fast').is(':checked');
+            self.optionFrom = $('#from').val() || '';
+            self.optionTo = $('#to').val() || '';
+            
+            var modal = $('.modal');
+            modal.find('span[data-length]').html(self.optionLength);
+            modal.find('span[data-width]').html(self.optionWidth);
+            modal.find('span[data-height]').html(self.optionHeight);
+            modal.find('span[data-weight]').html(self.optionWeight);
+            modal.find('span[data-date]').html(self.optionDate);            
+            modal.find('span[data-from]').html(self.optionFrom);
+            modal.find('span[data-to]').html(self.optionTo);
+            if(self.optionFast){
+                modal.find('span[data-fast]').html("срочная");
+            } else {
+                modal.find('span[data-fast]').html('обычная');
+            }
 		}
 		
 	};
 	
 	deliveryCalculator.init();
   
-  ///---
+  ///-----
   
   
     });
